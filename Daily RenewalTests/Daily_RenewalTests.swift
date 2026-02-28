c796935368ba7fcbbcec4c8f4a01233efafe4674
hiimthetwos <43576670+hiimthetwos@users.noreply.github.com>
2026-02-28 13:47:26 -0500

    second

---
+++ Daily RenewalTests/Daily_RenewalTests.swift
@@ -0,0 +1,16 @@
+//
+//  Daily_RenewalTests.swift
+//  Daily RenewalTests
+//
+//  Created by Ryan Bertuzzi on 2/25/26.
+//
+
+import Testing
+
+struct Daily_RenewalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Daily RenewalUITests/Daily_RenewalUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Daily_RenewalUITestsLaunchTests.swift
+//  Daily RenewalUITests
+//
+//  Created by Ryan Bertuzzi on 2/25/26.
+//
+
+import XCTest
+
+final class Daily_RenewalUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
